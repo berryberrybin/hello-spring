c9eaba657791fcc0ccf323021282ac87bcd39911
soobin <a01056239778@gmail.com>
2022-01-23 22:17:54 +0900

    #14 JPA 사용하도록 스프링 설정 변경

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
 // 도메인 : data class : getter, setter로 이루어져있음
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // @Column(name = "username")
     private String name;
 
 
